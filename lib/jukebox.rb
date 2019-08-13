6a1599f339713ea2a3abbc046408d9cdac9bf6c9
Marco McNulty <mmc90@hotmail.co.uk>
2019-08-13 16:30:38 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,8 +12,8 @@ songs = [
 
 def help
   puts "I accept the following commands:"
-- help : displays this help message
-- list : displays a list of songs you can play
+"- help : displays this help message"
+"- list : displays a list of songs you can play"
 - play : lets you choose a song to play
 - exit : exits this program
 end
